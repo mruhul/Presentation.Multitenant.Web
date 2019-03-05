2a07afe5f8d72b49c2a92cae6d2b165b9f548450
Mohammad Ruhul Amin <mohammadruhul.amin@carsales.com.au>
2019-03-05 12:58:42 +1100

    wip...

--- Presentation.Multitenant.Web/Features/Stocks/Providers/StaticDataSettings.cs
+++ Presentation.Multitenant.Web/Features/Stocks/Providers/StaticDataSettings.cs
@@ -1,8 +1,5 @@
 ﻿using Ingress.Core.Attributes;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Presentation.Multitenant.Web.Features.Stocks.Providers
 {
